3f71d355cad6888592f32df2270bce044452c804
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-22 13:04:31 +0100

    feat: add user to navbar when logged in

--- app/frontend/src/layout/DefaultLayout.tsx
+++ app/frontend/src/layout/DefaultLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/core/Navbar";
 import { Footer } from "@/components/core/Footer";
+import { useContext } from "react";
+import { AuthContext } from "@/context/Auth";
 
 export const DefaultLayout = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <>
-      <Navbar />
+      <Navbar user={user} />
       <div className="bg-slate-100 overflow-auto">
         <Outlet />
       </div>
