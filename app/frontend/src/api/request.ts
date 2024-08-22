2896786999ba58638930e3c2e72b3fbdf6bec6df
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-22 12:45:04 +0100

    refactor: update api request method

--- app/frontend/src/api/request.ts
+++ app/frontend/src/api/request.ts
@@ -1,42 +1,40 @@
 interface RequestOptions {
-    method?: string;
-    headers?: HeadersInit;
-    body?: BodyInit;
-    identity?: boolean;
+  method?: string;
+  headers?: HeadersInit;
+  body?: BodyInit;
+  identity?: boolean;
+}
+
+const apiUrl = import.meta.env.VITE_API_LOCAL_URL;
+
+export const request = async <T>(url: string, options: RequestOptions = {}) => {
+  const response = await fetch(
+    options.identity ? `${apiUrl}/${url}` : `${apiUrl}/api/v1/${url}`,
+    {
+      method: options.method,
+      headers: options.headers,
+      body: options.body,
+      credentials: "include"
+    }
+  );
+
+  if (response.status === 204) {
+    return {} as T;
   }
-  
-  const apiUrl = import.meta.env.VITE_API_LOCAL_URL;
-  
-  export const request = async <T>(url: string, options: RequestOptions = {}) => {
-    const response = await fetch(
-      options.identity ? `${apiUrl}/${url}` : `${apiUrl}/api/v1/${url}`,
-      {
-        method: options.method,
-        headers: options.headers,
-        body: options.body,
-        credentials: "include",
-      }
-    );
-  
-    if (response.status === 204) {
-      return {} as T;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const isJson = (response: any) => {
+    try {
+      JSON.parse(response);
+    } catch {
+      return false;
     }
-  
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const isJson = (response: any) => {
-      try {
-        JSON.parse(response);
-      } catch (e) {
-        console.error(e)
-        return false;
-      }
-      return true;
-    };
-  
-    if (isJson(response)) {
-      return response.json();
-    }
-  
-    return response;
+    return true;
   };
-  
+
+  if (isJson(response)) {
+    return response.json();
+  }
+
+  return response;
+};
