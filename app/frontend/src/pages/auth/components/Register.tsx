f30fbd69c5f3b54c28e690e4d61d9e5f1f5d23db
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-22 13:06:48 +0100

    feat: add register method to register form component

--- app/frontend/src/pages/auth/components/Register.tsx
+++ app/frontend/src/pages/auth/components/Register.tsx
@@ -5,26 +5,94 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { FormLayout } from "@/layout/FormLayout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AUTH_ROUTES } from "@/pages/routes";
 import { FormInput } from "@/components/forms/FormInput";
+import { useState } from "react";
+import { ResponseMessage } from "@/types";
+import { X } from "lucide-react";
+import { register } from "@/api/identity";
+import { getResponseErrors } from "@/utils";
+import { FormAlert } from "@/components/forms/FormAlert";
+import { Spinner } from "@/components/core/Loader";
+import { sendEmail } from "@/api/custom-auth";
+import { toast } from "sonner";
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [response, setResponse] = useState<ResponseMessage>({
+    name: "",
+    message: "",
+    errors: []
+  });
+  const [alert, setAlert] = useState<boolean>(false);
+
   const form = useForm<z.infer<typeof registerFormSchema>>({
     resolver: zodResolver(registerFormSchema),
     defaultValues: {
       email: "",
       password: "",
-      confirm: "",
-    },
+      confirm: ""
+    }
   });
 
-  function onSubmit(values: z.infer<typeof registerFormSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof registerFormSchema>) {
+    setIsLoading(true);
+
+    try {
+      const request = await register({
+        email: values.email,
+        password: values.password
+      });
+      if (request.ok) {
+        const send = await sendEmail(values.email, "register");
+        if (send.ok) {
+          navigate(AUTH_ROUTES.login);
+          toast.success(
+            "Registration Successful! Please check your email to confirm your account",
+            {
+              className: "bg-green-400",
+              duration: 3000
+            }
+          );
+        } else {
+          setResponse({
+            name: "error",
+            message: "Something went wrong! Please try again later"
+          });
+          setAlert(true);
+        }
+      } else {
+        const response = await request.json();
+        const errors = getResponseErrors(response.errors);
+        setResponse({ name: "error", message: response.title, errors: errors });
+        setAlert(true);
+      }
+    } catch (error) {
+      console.warn("Registration failed!");
+      console.error(error);
+    }
+
+    setIsLoading(false);
   }
+
+  const closeButton = (
+    <div className="cursor-pointer" onClick={() => setAlert(false)}>
+      <X className="h-4 w-4" />
+    </div>
+  );
 
   return (
     <FormLayout title="Create a new account">
+      {alert && (
+        <FormAlert
+          title={response.message}
+          type={response.name}
+          errors={response.errors}
+          closeButton={closeButton}
+        />
+      )}
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FormInput form={form} name="email" label="Email" />
@@ -44,7 +112,7 @@ export const Register = () => {
             type="submit"
             className="bg-blue-600 hover:bg-blue-500 mt-4 w-full"
           >
-            Submit
+            {isLoading ? <Spinner /> : "Submit"}
           </Button>
           <div className="flex mt-4 justify-center">
             <p className="text-sm text-slate-600">Already have an account?</p>
