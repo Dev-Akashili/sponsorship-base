f24c906cec7687211625a92ed5c169e823f4e75f
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-22 12:38:15 +0100

    style: format code according to prettier

--- app/frontend/src/api/identity.ts
+++ app/frontend/src/api/identity.ts
@@ -2,17 +2,17 @@ import { request } from "./request";
 
 const fetchKeys = {
   login: "login?useCookies=true&useSessionCookies=true",
-  logout: "logout",
+  logout: "logout"
 };
 
 export async function register(formData: { email: string; password: string }) {
   return await request("register", {
     method: "POST",
     headers: {
-      "Content-type": "application/json",
+      "Content-type": "application/json"
     },
     body: JSON.stringify(formData),
-    identity: true,
+    identity: true
   });
 }
 
@@ -20,10 +20,10 @@ export async function login(formData: { email: string; password: string }) {
   return await request(fetchKeys.login, {
     method: "POST",
     headers: {
-      "Content-type": "application/json",
+      "Content-type": "application/json"
     },
     body: JSON.stringify(formData),
-    identity: true,
+    identity: true
   });
 }
 
@@ -31,9 +31,9 @@ export async function logout() {
   return await request(fetchKeys.logout, {
     method: "POST",
     headers: {
-      "Content-type": "application/json",
+      "Content-type": "application/json"
     },
     body: JSON.stringify({}),
-    identity: true,
+    identity: true
   });
 }
