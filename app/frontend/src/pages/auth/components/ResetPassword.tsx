8ed398664154b4f5fbe9bb7b37b889bd54f3f940
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-22 13:09:31 +0100

    feat: add reset password form components and method

---
+++ app/frontend/src/pages/auth/components/ResetPassword.tsx
@@ -0,0 +1,233 @@
+import { FormLayout } from "@/layout/FormLayout";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  resetPasswordFormSchema,
+  verifyEmailFormSchema
+} from "./validationSchema";
+import { Form } from "@/components/ui/form";
+import { FormInput } from "@/components/forms/FormInput";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/core/Loader";
+import { useEffect, useState } from "react";
+import { resetPassword, sendEmail } from "@/api/custom-auth";
+import { toast } from "sonner";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { AUTH_ROUTES } from "@/pages/routes";
+import { ResponseMessage } from "@/types";
+import { FormAlert } from "@/components/forms/FormAlert";
+import { X } from "lucide-react";
+
+export const ResetPassword = () => {
+  const [searchParams] = useSearchParams();
+  const [form, setForm] = useState<string | null>("");
+  const [email, setEmail] = useState<string | null>("");
+  const [code, setCode] = useState<string | null>("");
+  const [codeId, setCodeId] = useState<string | null>("");
+
+  useEffect(() => {
+    setEmail(searchParams.get("email"));
+    setCode(searchParams.get("code"));
+    setCodeId(searchParams.get("codeId"));
+
+    if (email && code && codeId) {
+      setForm("reset");
+    } else {
+      setForm("verify");
+    }
+  }, [email, code, codeId, searchParams]);
+
+  const getForm = () => {
+    switch (form) {
+      case "reset":
+        return (
+          <ResetPasswordForm
+            code={code ?? ""}
+            codeId={Number(codeId)}
+            email={email ?? ""}
+          />
+        );
+      case "verify":
+        return <VerifyEmailForm />;
+      default:
+        return (
+          <div className="m-auto">
+            <Spinner size="lg" />
+          </div>
+        );
+    }
+  };
+
+  return <>{getForm()}</>;
+};
+
+const VerifyEmailForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof verifyEmailFormSchema>>({
+    resolver: zodResolver(verifyEmailFormSchema),
+    defaultValues: {
+      email: ""
+    }
+  });
+
+  const { reset } = form;
+
+  async function onSubmit(values: z.infer<typeof verifyEmailFormSchema>) {
+    setIsLoading(true);
+
+    try {
+      const send = await sendEmail(values.email, "reset");
+      if (send.ok) {
+        reset();
+        toast.success("Password reset link has been sent to your email", {
+          className: "bg-green-400",
+          duration: 3000
+        });
+      } else {
+        toast.error("Something went wrong! Please try again later", {
+          duration: 3000
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong! Please try again later", {
+        duration: 3000
+      });
+    }
+
+    setIsLoading(false);
+  }
+
+  return (
+    <FormLayout title="Verify email to reset password">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormInput form={form} name="email" label="Email" />
+          <Button
+            type="submit"
+            disabled={isLoading}
+            className="bg-blue-600 hover:bg-blue-500 mt-4 w-full"
+          >
+            {isLoading ? <Spinner /> : "Submit"}
+          </Button>
+          <div className="flex mt-4 justify-center">
+            <p className="text-sm text-slate-600">Return back to</p>
+            <Link to={AUTH_ROUTES.login}>
+              <p className="ml-2 text-sm text-blue-700 underline cursor-pointer">
+                Login
+              </p>
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </FormLayout>
+  );
+};
+
+interface ResetPasswordFormProps {
+  codeId: number;
+  code: string;
+  email: string;
+}
+
+const ResetPasswordForm = ({ codeId, code, email }: ResetPasswordFormProps) => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [response, setResponse] = useState<ResponseMessage>({
+    name: "",
+    message: ""
+  });
+  const [alert, setAlert] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof resetPasswordFormSchema>>({
+    resolver: zodResolver(resetPasswordFormSchema),
+    defaultValues: {
+      password: "",
+      confirm: ""
+    }
+  });
+
+  async function onSubmit(values: z.infer<typeof resetPasswordFormSchema>) {
+    setIsLoading(true);
+
+    try {
+      const request = await resetPassword({
+        codeId: codeId,
+        code: code,
+        email: email,
+        newPassword: values.password
+      });
+      if (request.ok) {
+        navigate(AUTH_ROUTES.login);
+        toast.success("Password has successfully been changed", {
+          className: "bg-green-400",
+          duration: 3000
+        });
+      } else {
+        const response = await request.json();
+        setResponse({
+          name: response.name,
+          message:
+            response.message === "expired"
+              ? "Link has expired"
+              : Array.isArray(response.message)
+              ? ""
+              : "Something went wrong! Please try again later",
+          errors: Array.isArray(response.message) ? response.message : []
+        });
+        setAlert(true);
+      }
+    } catch (error) {
+      console.error(error);
+      setResponse({
+        name: "error",
+        message: "Something went wrong! Please try again later"
+      });
+    }
+
+    setIsLoading(false);
+  }
+
+  const closeButton = (
+    <div className="cursor-pointer" onClick={() => setAlert(false)}>
+      <X className="h-4 w-4" />
+    </div>
+  );
+
+  return (
+    <FormLayout title="Reset password">
+      {alert && (
+        <FormAlert
+          title={response.message}
+          type={response.name}
+          closeButton={closeButton}
+        />
+      )}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormInput
+            form={form}
+            name="password"
+            label="Password"
+            type="password"
+          />
+          <FormInput
+            form={form}
+            name="confirm"
+            label="Confirm Password"
+            type="password"
+          />
+          <Button
+            type="submit"
+            disabled={isLoading}
+            className="bg-blue-600 hover:bg-blue-500 mt-4 w-full"
+          >
+            {isLoading ? <Spinner /> : "Reset password"}
+          </Button>
+        </form>
+      </Form>
+    </FormLayout>
+  );
+};
--- app/frontend/src/pages/auth/components/validationSchema.tsx
+++ app/frontend/src/pages/auth/components/validationSchema.tsx
@@ -1,9 +1,34 @@
 import { z } from "zod";
 
+export const verifyEmailFormSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required")
+});
+
 export const loginFormSchema = z.object({
   email: z.string().email("Invalid email address").min(1, "Email is required"),
-  password: z.string().min(1, "Password is required"),
+  password: z.string().min(1, "Password is required")
 });
+
+export const resetPasswordFormSchema = z
+  .object({
+    password: z
+      .string()
+      .min(6, { message: "Passwords must be at least 6 characters." })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Passwords must have at least one non alphanumeric character."
+      })
+      .regex(/\d/, {
+        message: "Passwords must have at least one digit ('0'-'9')."
+      })
+      .regex(/[A-Z]/, {
+        message: "Passwords must have at least one uppercase ('A'-'Z')."
+      }),
+    confirm: z.string().min(1, "Confirm Password is required")
+  })
+  .refine((data) => data.password === data.confirm, {
+    path: ["confirm"],
+    message: "Passwords do not match"
+  });
 
 export const registerFormSchema = z
   .object({
@@ -11,10 +36,21 @@ export const registerFormSchema = z
       .string()
       .email("Invalid email address")
       .min(1, "Email is required"),
-    password: z.string().min(1, "Password is required"),
-    confirm: z.string().min(1, "Confirm Password is required"),
+    password: z
+      .string()
+      .min(6, { message: "Passwords must be at least 6 characters." })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Passwords must have at least one non alphanumeric character."
+      })
+      .regex(/\d/, {
+        message: "Passwords must have at least one digit ('0'-'9')."
+      })
+      .regex(/[A-Z]/, {
+        message: "Passwords must have at least one uppercase ('A'-'Z')."
+      }),
+    confirm: z.string().min(1, "Confirm Password is required")
   })
   .refine((data) => data.password === data.confirm, {
     path: ["confirm"],
-    message: "Passwords do not match",
+    message: "Passwords do not match"
   });
