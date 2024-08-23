9fa20677a004b5189e9d9d41af6fc631cb5694f2
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-23 11:47:15 +0100

    style: change global font family to Moderustic

--- app/frontend/src/components/core/Helmet.tsx
+++ app/frontend/src/components/core/Helmet.tsx
@@ -5,7 +5,9 @@ export const Helmet = () => {
     <Link to={"/"}>
       <div className="flex gap-2 items-center hover:cursor-pointer">
         <img className="h-12 w-12" src="././globe.png" alt="globe" />
-        <h1 className="text-gray-800 text-2xl font-medium">SponsorshipBase</h1>
+        <h1 className="text-gray-800 text-2xl font-semibold">
+          SponsorshipBase
+        </h1>
       </div>
     </Link>
   );
--- app/frontend/src/pages/home/IndexPage.tsx
+++ app/frontend/src/pages/home/IndexPage.tsx
@@ -11,18 +11,18 @@ export const IndexPage = () => {
           <span className="text-blue-600 hover:mb-4">visa sponsorsed</span> jobs
         </h1>
         <p className="mx-auto my-10 text-center text-slate-600 text-xl w-[70%]">
-          Looking for a job that offers visa sponsorship can be dauntiing! This
+          Looking for a job that offers visa sponsorship can be daunting! This
           platform is built to help provide information from people that have
           pulled it off and serve as a guide.
         </p>
         <div className="text-center">
           <Link to={ROUTES.sponsorshipList}>
-            <Button className="mx-2 bg-blue-600 hover:bg-blue-500 font-mono">
+            <Button className="mx-2 bg-blue-600 hover:bg-blue-500">
               View Sponsorship list
             </Button>
           </Link>
           <Link to={ROUTES.contribute}>
-            <Button className="mx-2 bg-blue-600 hover:bg-blue-500 font-mono">
+            <Button className="mx-2 bg-blue-600 hover:bg-blue-500">
               Add Your Sponsored Job
             </Button>
           </Link>
