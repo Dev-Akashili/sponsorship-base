32c7193f0d0412c33b54473f73b14fab54adaf25
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-08-29 11:57:38 +0100

    ci: update docs site deploy workflow

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/sponsorship-base",
+  assetPrefix: "/sponsorship-base",
+  output: "export",
+  images: {
+    unoptimized: true
+  }
+};
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx"
 });
 
-module.exports = withNextra();
+module.exports = withNextra(nextConfig);
