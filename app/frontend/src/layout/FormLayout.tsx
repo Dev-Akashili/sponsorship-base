002de7bbc2a0c734f8910d31c96684e3b1d90d75
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-09-02 15:03:46 +0100

    refactor: improve form layout ui

--- app/frontend/src/layout/FormLayout.tsx
+++ app/frontend/src/layout/FormLayout.tsx
@@ -2,16 +2,27 @@ import { ReactNode } from "react";
 
 interface FormLayoutProps {
   title?: string;
+  size?: "bs" | "lg";
   children: ReactNode | ReactNode[];
 }
 
-export const FormLayout = ({ title, children }: FormLayoutProps) => {
+export const FormLayout = ({
+  title,
+  size = "bs",
+  children
+}: FormLayoutProps) => {
   return (
-    <div className="h-screen flex justify-center align-center">
-      <div className="w-96 m-auto">
-        <h1 className="text-center text-2xl text-slate-600 font-medium my-4">
-          {title}
-        </h1>
+    <div
+      className={`${
+        size === "lg" ? "h-full" : "h-screen"
+      } flex justify-center align-center`}
+    >
+      <div className={`${size === "lg" ? "w-110" : "w-96"} m-auto`}>
+        {title && (
+          <h1 className="text-center text-2xl text-slate-600 font-medium my-4">
+            {title}
+          </h1>
+        )}
         <div className=" p-6 m-auto rounded-lg bg-white">{children}</div>
       </div>
     </div>
