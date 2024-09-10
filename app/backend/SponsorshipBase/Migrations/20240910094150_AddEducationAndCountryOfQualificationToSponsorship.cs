536b7297ba0980b02795b57c3a292ad3b56d1f9c
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-09-10 13:59:10 +0100

    chore: add migrations

---
+++ app/backend/SponsorshipBase/Migrations/20240910094150_AddEducationAndCountryOfQualificationToSponsorship.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SponsorshipBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEducationAndCountryOfQualificationToSponsorship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryOfQualification",
+                table: "Sponsorships",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Education",
+                table: "Sponsorships",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryOfQualification",
+                table: "Sponsorships");
+
+            migrationBuilder.DropColumn(
+                name: "Education",
+                table: "Sponsorships");
+        }
+    }
+}
