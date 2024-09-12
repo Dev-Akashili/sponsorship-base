ffcef40344ef557ac5190f16c0899e15cf8fa12d
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-09-12 22:29:27 +0100

    chore: add migrations

---
+++ app/backend/SponsorshipBase/Migrations/20240912162604_AddFavouritesToSponsorship.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SponsorshipBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavouritesToSponsorship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Favourites",
+                table: "Sponsorships",
+                type: "text[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Favourites",
+                table: "Sponsorships");
+        }
+    }
+}
