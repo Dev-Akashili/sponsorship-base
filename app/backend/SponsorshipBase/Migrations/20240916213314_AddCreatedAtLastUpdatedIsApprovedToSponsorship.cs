2600e970dd573c2a32feed2a80e5237999d87a28
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-09-17 01:12:31 +0100

    chore: add migrations

---
+++ app/backend/SponsorshipBase/Migrations/20240916213314_AddCreatedAtLastUpdatedIsApprovedToSponsorship.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SponsorshipBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtLastUpdatedIsApprovedToSponsorship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logo",
+                table: "Companies");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Sponsorships",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Sponsorships",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Sponsorships",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Sponsorships");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Sponsorships");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Sponsorships");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Logo",
+                table: "Companies",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
