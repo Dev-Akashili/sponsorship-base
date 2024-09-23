08c0e385de10618d7786783a515012df679d2399
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-09-23 18:34:03 +0100

    chore: add migrations

--- app/backend/SponsorshipBase/Migrations/20240923172040_AddMinimumSalaryToSponsorship.Designer.cs
+++ app/backend/SponsorshipBase/Migrations/20240923172040_AddMinimumSalaryToSponsorship.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SponsorshipBase.Data;
@@ -12,9 +13,11 @@ using SponsorshipBase.Data;
 namespace SponsorshipBase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240923172040_AddMinimumSalaryToSponsorship")]
+    partial class AddMinimumSalaryToSponsorship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ app/backend/SponsorshipBase/Migrations/20240923172040_AddMinimumSalaryToSponsorship.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SponsorshipBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMinimumSalaryToSponsorship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumSalary",
+                table: "Sponsorships",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumSalary",
+                table: "Sponsorships");
+        }
+    }
+}
