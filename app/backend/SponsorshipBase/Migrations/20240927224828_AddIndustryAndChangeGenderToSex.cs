1afb737225015e380bf3ff5a1e23bd7c67c9dbdf
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-10-01 12:01:20 +0100

    chore: add migrations

---
+++ app/backend/SponsorshipBase/Migrations/20240927224828_AddIndustryAndChangeGenderToSex.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SponsorshipBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndustryAndChangeGenderToSex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Sponsorships",
+                newName: "Sex");
+
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "AspNetUsers",
+                newName: "Sex");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Industry",
+                table: "Sponsorships",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Industry",
+                table: "Sponsorships");
+
+            migrationBuilder.RenameColumn(
+                name: "Sex",
+                table: "Sponsorships",
+                newName: "Gender");
+
+            migrationBuilder.RenameColumn(
+                name: "Sex",
+                table: "AspNetUsers",
+                newName: "Gender");
+        }
+    }
+}
